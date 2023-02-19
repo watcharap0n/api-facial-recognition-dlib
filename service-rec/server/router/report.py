f3df17ef7ff1f9245203b6977b096fa5f312ae86
watcharapon weeraborirak <watcharapon.w@thaicom.net>
2023-02-19 19:04:50 +0700

    report.py

--- service-rec/server/router/report.py
+++ service-rec/server/router/report.py
@@ -8,12 +8,19 @@ from ..schemas.other import SuccessRequest
 from ..schemas.log import log_transaction, StatusTraining
 
 router = APIRouter()
+ROOT_DIR = os.path.abspath('.')
+ROOT_DIR = os.path.join(ROOT_DIR, 'assets')
 
 
 @router.get('/get-models', response_model=SuccessRequest)
-async def get_models_from_directory():
-    root_dir = os.path.abspath('.')
-    dir_models = os.path.join(root_dir, 'trained_models')
+async def get_models_from_directory(background_task: BackgroundTasks):
+    dir_models = os.path.join(ROOT_DIR, 'trained_models')
+
+    background_task.add_task(
+        log_transaction,
+        method='/GET',
+        endpoint='/get-models',
+    )
     return {'status': True, 'detail': {'list_model': os.listdir(dir_models)}}
 
 
@@ -24,28 +31,28 @@ async def get_models_from_directory():
 )
 async def report_training_status(
         background_task: BackgroundTasks,
-        file_trained_model: str,
+        uuid: str,
         redis_conn=Depends(init_redis)
 ):
+    redis_model = await redis_conn.get(uuid)
+    if redis_model is None:
+        raise HTTPException(status_code=status.HTTP_200_OK,
+                            detail='Not in progress train model.')
+    redis_model = json.loads(redis_model)
+
     background_task.add_task(
         log_transaction,
         method='/GET',
         endpoint='/report/training',
     )
-    redis_model = await redis_conn.get(file_trained_model)
-    if redis_model is None:
-        raise HTTPException(status_code=status.HTTP_200_OK,
-                            detail='Not in progress train model.')
-    redis_model = json.loads(redis_model)
     return redis_model
 
 
 @router.post('/cropped/complete', response_model=ReportCropped)
-async def return_error_cropped_image(config: ReportConfig):
-    root_dir = os.path.abspath('.')
+async def return_error_cropped_image(config: ReportConfig, background_task: BackgroundTasks):
     try:
-        datasets = os.path.join(root_dir, config.datasets_folder)
         details_missing = []
+        datasets = os.path.join(ROOT_DIR, config.datasets_folder)
         for data in os.listdir(datasets):
             if not data.startswith('.'):
                 path_id = os.path.join(datasets, data)
@@ -72,4 +79,10 @@ async def return_error_cropped_image(config: ReportConfig):
         return model_store
     model_store.status = False
     model_store.detail = details_missing
+
+    background_task.add_task(
+        log_transaction,
+        method='/POST',
+        endpoint='/cropped/complete',
+    )
     return model_store
