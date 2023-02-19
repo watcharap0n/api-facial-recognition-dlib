944042d8a5af0bec6d74ccab020adc63da9284a2
watcharapon weeraborirak <watcharapon.w@thaicom.net>
2023-02-19 19:05:53 +0700

    configuration

--- service-rec/server/dependencies/__init__.py
+++
@@ -1,1 +0,0 @@
-from .memory import DATA_CACHE
--- service-rec/server/engine/__init__.py
+++ service-rec/server/engine/__init__.py
@@ -2,4 +2,5 @@ import os
 import dlib
 from .crop_extraction import crop_extraction_faces
 from .face_extraction import train_model_facials
-from .model import Config, ReportConfig
+from .model import Config, ReportConfig, ConfigTrain, ConfigCrop
+from .predict_image import predict
